a1ce6d23772d7800584cae1af87cb5bfe57896b0
Bilik Tamás <biliktamas79@gmail.com>
2019-01-26 01:41:47 +0100

    Added caching to AppendFriendlyTypeName extension.

--- src/Multi.Core/Extensions.StringBuilder.cs
+++ src/Multi.Core/Extensions.StringBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using Multi;
 
@@ -6,6 +7,8 @@ namespace System
 {
     public static partial class Extensions
     {
+        internal static Dictionary<ValueTuple<RuntimeTypeHandle, TypeNameStringShorteningFlags>, string> _friendlyGenericTypeNameCache = new Dictionary<ValueTuple<RuntimeTypeHandle, TypeNameStringShorteningFlags>, string>();
+
         public static StringBuilder AppendFriendlyTypeName(this StringBuilder sb, Type type, TypeNameStringShorteningFlags typeNameShorteningFlags = TypeNameStringShorteningFlags.All)
         {
             if (sb == null)
@@ -20,36 +23,45 @@ namespace System
                 Type[] genArgTypes = type.GenericTypeArguments;
                 if ((genArgTypes != null) && (genArgTypes.Length > 0))
                 {
+                    var cacheKey = ValueTuple.Create(type.TypeHandle, typeNameShorteningFlags);
+                    if (_friendlyGenericTypeNameCache.TryGetValue(cacheKey, out var cachedString))
+                        return sb.Append(cachedString);
+
+                    StringBuilder cacheStringBuilder = new StringBuilder();
                     if (typeNameShorteningFlags.HasFlag(TypeNameStringShorteningFlags.ShortNullableTypeNames) && type.FullName.StartsWith("System.Nullable`1"))
                         //sb.Append(genArgTypes[0].Name).Append("?");
-                        AppendFriendlyTypeName(sb, genArgTypes[0], typeNameShorteningFlags).Append("?");
+                        AppendFriendlyTypeName(cacheStringBuilder, genArgTypes[0], typeNameShorteningFlags).Append("?");
                     else
                     {
                         bool selfContaining = false;
                         int firstIndex = typeName.IndexOf('`');
 
-                        sb.Append((firstIndex >= 0) ? typeName.Substring(0, firstIndex) : typeName);
-                        sb.Append("<");
+                        cacheStringBuilder.Append((firstIndex >= 0) ? typeName.Substring(0, firstIndex) : typeName);
+                        cacheStringBuilder.Append("<");
                         bool appendSeparator = false;
                         foreach (Type genArgType in genArgTypes)
                         {
                             if (appendSeparator)
-                                sb.Append(", ");
+                                cacheStringBuilder.Append(", ");
                             if (genArgType.Equals(type))
                             {
                                 selfContaining = true;
-                                sb.Append("{0}");
+                                cacheStringBuilder.Append("{0}");
                             }
                             else
                             {
-                                AppendFriendlyTypeName(sb, genArgType, typeNameShorteningFlags);
+                                AppendFriendlyTypeName(cacheStringBuilder, genArgType, typeNameShorteningFlags);
                             }
                             appendSeparator = true;
                         }
-                        sb.Append(">");
+                        cacheStringBuilder.Append(">");
                         if (selfContaining)
                             throw new NotSupportedException("Generic classes containing themself as a generic argument are not supported!");
                     }
+
+                    var cachedTypeName = cacheStringBuilder.ToString();
+                    _friendlyGenericTypeNameCache[cacheKey] = cachedTypeName;
+                    sb.Append(cachedTypeName);
                 }
             }
             else
